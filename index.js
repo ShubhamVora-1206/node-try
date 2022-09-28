e1c8805954d72a6db33eb76db0176fca46783f54
Shubham Vora <shubhamv3108@gmail.com>
2022-09-28 19:11:44 +0530

    commit 5

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 const port = process.env.PORT || 80;
 function reqHandler(req,res){
     res.writeHead(200,{"Content-Type":"text/plain"});
-    res.end("Hello World")
+    res.end("Kundi Laga lo saiyaaaa, Tumharko Jannat dikhati..")
 }
 
 const server = http.createServer(reqHandler);
